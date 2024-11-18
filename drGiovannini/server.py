371cfeae9ad6926e27cb497adb0f118946232c96
zhousi87 <stefanogiovannini87@gmail.com>
2024-11-18 18:13:38 +0100

    returned to previous but putting new routes at the bottom, outside templating decorations

--- drGiovannini/server.py
+++ drGiovannini/server.py
@@ -2,15 +2,11 @@ from datetime import datetime
 
 from flask import Flask, render_template, request, send_from_directory
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 current_year = datetime.now().year
 
 @app.route('/')
-@app.route('/robots.txt')
-@app.route('/sitemap.xml')
-def static_from_root():
-    return send_from_directory(app.static_folder, request.path[1:])
 @app.route('/<route_name>')
 def home(route_name=None):
     if route_name is None:
@@ -45,6 +41,10 @@ def home(route_name=None):
             template_name_or_list='404.html',
             current_year=current_year
         )
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 if __name__ == '__main__':
     app.run(debug=False) # set to True in development, False when publishing on remote repositories and in production
