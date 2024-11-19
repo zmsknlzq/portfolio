72dd331559f9ee27d1180b3e4d52ee83b1f0f834
zhousi87 <stefanogiovannini87@gmail.com>
2024-11-19 14:57:42 +0100

    added metadescrptions

--- drGiovannini/server.py
+++ drGiovannini/server.py
@@ -13,41 +13,55 @@ def home(route_name=None):
         return render_template(
             template_name_or_list='welcome.html',
             current_year=current_year, # for keeping copyright up to date
-            page_title='Dr Stefano Giovannini'
+            page_title='Dr Stefano Giovannini',
+            meta_description='Dr Stefano Giovannini is an Italian native fluent in English and Mandarin Chinese. '
+                             'He provides language-teaching, translation, Python-programming, and housing-related '
+                             'services. He is the Vice President of the Pedone Association for '
+                             'the Intercultural Exchange (pedone.eu).'
         )
     elif route_name.lower() == 'bio':
         return render_template(
             template_name_or_list='bio.html',
             current_year=current_year,
-            page_title='Dr Stefano Giovannini\'s bio'
+            page_title='Dr Stefano Giovannini\'s bio',
+            meta_description='Dr Stefano Giovannini has a PhD in Digital Humanities, is fluent in English and Chinese, '
+                             'is a skilled Python programmer, and is the Vice President of the Pedone Association for '
+                             'the Intercultural Exchange (pedone.eu). He provides services related to language teaching,'
+                             ' translation, Python programming, and housing in Lombardy.'
         )
     elif route_name.lower() == 'email':
         return render_template(
             template_name_or_list='email.html',
             current_year=current_year,
-            page_title='Dr Stefano Giovannini\'s e-mail'
+            page_title='Dr Stefano Giovannini\'s e-mail',
+            meta_description='Dr Stefano Giovannini\'s e-mail for inquiries related to language teaching,'
+                             ' translation, Python programming, and housing in Lombardy.'
         )
     elif route_name.lower() == 'pedone':
         return render_template(
             template_name_or_list='pedone.html',
             current_year=current_year,
-            page_title='Dr Stefano Giovannini Vice President of Pedone Association'
+            page_title='Dr Stefano Giovannini Vice President of Pedone Association',
+            meta_description='Dr Stefano Giovannini is the Vice President of the Pedone Association for '
+                             'the Intercultural Exchange (pedone.eu), whose mission is the promotion of Chinese art '
+                             'among Westerners and Western art among Chinese.'
         )
     elif route_name.lower() == 'services':
         return render_template(
             template_name_or_list='services.html',
             current_year=current_year,
-            page_title='Dr Stefano Giovannini\'s services'
+            page_title='Dr Stefano Giovannini\'s services',
+            meta_description='Dr Stefano Giovannini provides services related to language teaching,'
+                             ' translation, Python programming, and housing in Lombardy. This page lists the most '
+                             'frequently requested among them.'
         )
-    # elif route_name.lower() == 'sitemap':
-    #     return 'sitemap.xml'
-    # elif route_name.lower() == 'robots':
-    #     return 'robots.txt'
     else:
         return render_template(
             template_name_or_list='404.html',
             current_year=current_year,
-            page_title='Dr Stefano Giovannini - 404'
+            page_title='Dr Stefano Giovannini - 404',
+            meta_description='Dr Stefano Giovannini uses this page to notify the user that the requested route does not'
+                             ' exist on drgiovannini.com.'
         )
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
